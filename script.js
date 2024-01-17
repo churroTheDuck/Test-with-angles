33f5ea8c36a989c9cce953fee3b16ab719f44b4c
Lang Chen <langchenca@gmail.com>
2024-01-16 23:05:31 -0800

    C

--- script.js
+++ script.js
@@ -11,17 +11,19 @@ function setup() {
 
 function draw() {
   background(0);
+  translate(0, 0, 0);
+  box(1000);
   if (shoot) {
     bulletsX.push(0);
     bulletsY.push(0);
     bulletsZ.push(0);
     bulletsDir.push(atan2(mouseY - height / 2, mouseX - width / 2));
   }
-  for (var i = 0; i < bulletsX.length, i += 1;) {
+  for (var i = 0; i < bulletsX.length; i += 1) {
     push();
     translate(bulletsX[i], bulletsY[i], bulletsZ[i]);
     cylinder(20, 1, 1, 1);
-    bulletsZ -= 1;
+    bulletsZ[i] -= 1;
     pop();
   }
 }
